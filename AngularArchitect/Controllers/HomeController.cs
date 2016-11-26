579b637aa9fde3f9530508589882fe33dbef62a1
atul221282 <atul221282@gmail.com>
2016-11-26 16:47:01 +1030

    create minimum mvc project

---
+++ AngularArchitect/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularArchitect.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
